93b122787468bd218cacbbd25f2e538d7d7571f6
Kevin Ushey <kevin@rstudio.com>
2015-01-27 13:01:00 -0800

    disable ace token merging in markdown

--- src/gwt/acesupport/acemode/markdown_highlight_rules.js
+++ src/gwt/acesupport/acemode/markdown_highlight_rules.js
@@ -62,14 +62,6 @@ var escaped = function(ch) {
     return "(?:[^" + lang.escapeRegExp(ch) + "\\\\]|\\\\.)*";
 }
 
-function github_embed(tag, prefix) {
-    return { // Github style block
-        token : "support.function",
-        regex : "^\\s*```" + tag + "\\s*$",
-        push  : prefix + "start"
-    };
-}
-
 var MarkdownHighlightRules = function() {
     HtmlHighlightRules.call(this);
 
@@ -84,50 +76,44 @@ var MarkdownHighlightRules = function() {
     this.$rules["start"].unshift({
         token : "empty_line",
         regex : '^$',
+        merge : false,
         next: "allowBlock"
     }, { // code span
         token : ["support.function", "support.function", "support.function"],
+        merge : false,
         regex : "(`+)([^\\r]*?[^`])(\\1)"
     }, { // h1 with equals
         token: "markup.heading.1",
         regex: "^\\={3,}\\s*$",
+        merge : false,
         next: "fieldblock"
     }, { // h1
         token: "markup.heading.1",
+        merge : false,
         regex: "^=+(?=\\s*$)"
     }, { // h2
         token: "markup.heading.2",
+        merge : false,
         regex: "^\\-+(?=\\s*$)"
     }, {
         token : function(value) {
             return "markup.heading." + value.length;
         },
         regex : /^#{1,6}(?=\s*[^ #]|\s+#.)/,
+        merge : false,
         next : "header"
-    },
-                                 
-    github_embed("(?:javascript|js)", "jscode-"),
-    github_embed("xml", "xmlcode-"),
-    github_embed("html", "htmlcode-"),
-    github_embed("css", "csscode-"),
-    
-    { // Github style block
-        token : "support.function",
-        regex : "^\\s*```\\s*\\S*(?:{.*?\\})?\\s*$",
-        next  : "githubblock"
-    }, { // block quote
-        token : "string.blockquote",
-        regex : "^\\s*>\\s*(?:[*+-]|\\d+\\.)?\\s+",
-        next  : "blockquote"
     },
     { // reference
         token : ["text", "constant", "text", "url", "string", "text"],
+        merge : false,
         regex : "^([ ]{0,3}\\[)([^\\]]+)(\\]:\\s*)([^ ]+)(\\s*(?:[\"][^\"]+[\"])?(\\s*))$"
     }, { // link by reference
         token : ["text", "keyword", "text", "constant", "text"],
+        merge : false,
         regex : "(\\[)((?:[[^\\]]*\\]|[^\\[\\]])*)(\\][ ]?(?:\\n[ ]*)?\\[)(.*?)(\\])"
     }, { // link by url
         token : ["text", "keyword", "text", "markup.underline", "string", "text"],
+        merge : false,
         regex : "(\\[)"+
             "(\\[[^\\]]*\\]|[^\\[\\]]*)"+
             "(\\]\\([ \\t]*)"+
@@ -136,39 +122,50 @@ var MarkdownHighlightRules = function() {
             "(\\))"
     }, { // HR *
         token : "constant",
+        merge : false,
         regex : "^[ ]{0,2}(?:[ ]?\\*[ ]?){3,}\\s*$"
     }, { // HR -
         token : "constant",
+        merge : false,
         regex : "^[ ]{0,2}(?:[ ]?\\-[ ]?){3,}\\s*$"
     }, { // HR _
         token : "constant",
+        merge : false,
         regex : "^[ ]{0,2}(?:[ ]?\\_[ ]?){3,}\\s*$"
     }, { // MathJax native display \[ ... \]
         token : "markup.list",
+        merge : false,
         regex : "\\\\\\[",
         next  : "mathjaxnativedisplay"
     }, { // MathJax native inline \( ... \)
         token : "markup.list",
+        merge : false,
         regex : "\\\\\\(",
         next  : "mathjaxnativeinline"
     }, { // $ escape
         token : "text",
+        merge : false,
         regex : "\\\\\\$"
     }, { // MathJax $$
         token : "markup.list",
+        merge : false,
         regex : "\\${2}",
         next  : "mathjaxdisplay"
     }, { // MathJax $...$ (org-mode style)
         token : ["markup.list","support.function","markup.list"],
+        merge : false,
         regex : "(\\$)" + "((?!\\s)[^$]*[^$\\s])" + "(\\$)" + "(?![\\w\\d`])"
     }, { // strong ** __
         token : ["constant.numeric", "constant.numeric", "constant.numeric"],
+        merge : false,
         regex : "([*]{2}|[_]{2}(?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
     }, { // emphasis * _
         token : ["constant.language.boolean", "constant.language.boolean", "constant.language.boolean"],
+        merge : false,
         regex : "([*]|[_](?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
     }, { // simple links <url>
         token : ["text", "keyword", "text"],
+        merge : false,
         regex : "(<)("+
             "(?:https?|ftp|dict):[^'\">\\s]+"+
             "|"+
@@ -177,31 +174,39 @@ var MarkdownHighlightRules = function() {
     }, {
         // embedded latex command
         token : "keyword",
+        merge : false,
         regex : "\\\\(?:[a-zA-z0-9]+|[^a-zA-z0-9])"
     }, {
         // embedded latex arg
         token : ["paren.keyword.operator", "text", "paren.keyword.operator"],
+        merge : false,
         regex : "(\\{)([^\\}]*)(\\})"
     }, {
         // pandoc citation with brackets
         token : "markup.list",
+        merge : false,
         regex : "\\[-?\\@[\\w\\d-]+\\]"
     }, {
         // pandoc citation
         token : "markup.list",
+        merge : false,
         regex : "-?\\@[\\w\\d-]+"
     }, {
         token : "text",
+        merge : false,
         regex : "[^\\*_%$`\\[#<>\\\\]+"
     }, {
         token : "text",
+        merge : false,
         regex : "\\\\"
     }, { // HR * - _
         token : "constant",
+        merge : false,
         regex : "^ {0,2}(?:(?: ?\\* ?){3,}|(?: ?\\- ?){3,}|(?: ?\\_ ?){3,})\\s*$",
         next: "allowBlock"
     }, { // list
         token : "text",
+        merge : false,
         regex : "^\\s{0,3}(?:[*+-]|\\d+\\.)\\s+",
         next  : "listblock-start"
     }, {
@@ -211,18 +216,23 @@ var MarkdownHighlightRules = function() {
     this.addRules({
         "basic" : [{
             token : "constant.language.escape",
+            merge : false,
             regex : /\\[\\`*_{}\[\]()#+\-.!]/
         }, { // code span `
             token : ["support.function", "support.function", "support.function"],
+            merge : false,
             regex : "(`+)(.*?[^`])(\\1)"
         }, { // reference
             token : ["text", "constant", "text", "url", "string", "text"],
+            merge : false,
             regex : "^([ ]{0,3}\\[)([^\\]]+)(\\]:\\s*)([^ ]+)(\\s*(?:[\"][^\"]+[\"])?(\\s*))$"
         }, { // link by reference
             token : ["text", "keyword", "text", "constant", "text"],
+            merge : false,
             regex : "(\\[)(" + escaped("]") + ")(\\]\s*\\[)("+ escaped("]") + ")(\\])"
         }, { // link by url
             token : ["text", "keyword", "text", "markup.underline", "string", "text"],
+            merge : false,
             regex : "(\\[)(" +                                        // [
                 escaped("]") +                                    // link text
                 ")(\\]\\()"+                                      // ](
@@ -231,12 +241,15 @@ var MarkdownHighlightRules = function() {
                 "(\\))"                                           // )
         }, { // strong ** __
             token : ["constant.numeric", "constant.numeric", "constant.numeric"],
+            merge : false,
             regex : "([*]{2}|[_]{2}(?=\\S))(.*?\\S[*_]*)(\\1)"
         }, { // emphasis * _
             token : ["constant.language.boolean", "constant.language.boolean", "constant.language.boolean"],
+            merge : false,
             regex : "([*]|[_](?=\\S))(.*?\\S[*_]*)(\\1)"
         }, { //
             token : ["text", "keyword", "text"],
+            merge : false,
             regex : "(<)("+
                 "(?:https?|ftp|dict):[^'\">\\s]+"+
                 "|"+
@@ -245,13 +258,20 @@ var MarkdownHighlightRules = function() {
         }],
 
         // code block
-        "allowBlock": [
-            {token : "support.function", regex : "^ {4}.+", next : "allowBlock"},
-            {token : "empty", regex : "", next : "start"}
-        ],
+        "allowBlock": [{
+            token : "support.function",
+            merge : false,
+            regex : "^ {4}.+", next : "allowBlock"
+        }, {
+            token : "empty",
+            merge : false,
+            regex : "",
+            next : "start"
+        }],
 
         "header" : [{
             regex: "$",
+            merge : false,
             next : "start"
         }, {
             include: "basic"
@@ -261,50 +281,48 @@ var MarkdownHighlightRules = function() {
 
         "listblock-start" : [{
             token : "text",
+            merge : false,
             regex : /(?:\[[ x]\])?/,
             next  : "listblock"
         }],
 
         "listblock" : [ { // Lists only escape on completely blank lines.
             token : "empty_line",
+            merge : false,
             regex : "^$",
             next  : "start"
         }, { // list
             token : "text",
+            merge : false,
             regex : "^\\s{0,3}(?:[*+-]|\\d+\\.)\\s+",
             next  : "listblock-start"
         }, {
             include : "basic", noEscape: true
         }, { // Github style block
             token : "support.function",
+            merge : false,
             regex : "^\\s*```\\s*[a-zA-Z]*(?:{.*?\\})?\\s*$",
             next  : "githubblock"
         }, {
             defaultToken : "text" //do not use markup.list to allow stling leading `*` differntly
         } ],
 
-        "blockquote" : [ { // Blockquotes only escape on blank lines.
+        "blockquote" : [{ // Blockquotes only escape on blank lines.
             token : "empty_line",
+            merge : false,
             regex : "^\\s*$",
             next  : "start"
         }, { // block quote
             token : "string.blockquote",
+            merge : false,
             regex : "^\\s*>\\s*(?:[*+-]|\\d+\\.)?\\s+",
             next  : "blockquote"
         }, {
             include : "basic", noEscape: true
         }, {
             defaultToken : "string.blockquote"
-        } ],
+        }]
 
-        "githubblock" : [ {
-            token : "support.function",
-            regex : "^\\s*```",
-            next  : "start"
-        }, {
-            token : "support.function",
-            regex : ".+"
-        } ]
     });
 
     this.embedRules(JavaScriptHighlightRules, "jscode-", [{
